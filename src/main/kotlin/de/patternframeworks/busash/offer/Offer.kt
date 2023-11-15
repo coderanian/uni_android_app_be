301d56cee69f95b3bafc31d85c27dfd49b008839
wrigglingmoray <konstanntin@gmail.com>
2023-11-15 18:02:38 +0100

    Post, Get, Put and Delete for Offer; Get for Profile updated with functional offerCnt; Delete for Offer calls function to increment offerCnt of user.

--- src/main/kotlin/de/patternframeworks/busash/offer/Offer.kt
+++ src/main/kotlin/de/patternframeworks/busash/offer/Offer.kt
@@ -10,9 +10,11 @@ data class Offer(
     @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "offer_id") val offerId: Long,
     @Column(name = "title") val title: String,
     @Column(name = "description") val description: String? = "",
+    @Enumerated(EnumType.STRING) @Column(name = "category") val category: OfferCategory,
     @Column(name = "quantity") val quantity: String? = "",
     @Enumerated(EnumType.STRING) @Column(name = "price_type") val priceType: PriceType,
     @Column(name = "price") val price: String,
     @Column(name ="product_pic") val productPic: String? = "",
-    @ManyToOne @JoinColumn(name = "author_id") val author: User
+    //Information is extracted from authroization header, so we have to allow it being absent from request header
+    @ManyToOne @JoinColumn(name = "author_id") var author: User? = null
 )
--- src/main/kotlin/de/patternframeworks/busash/offer/OfferController.kt
+++ src/main/kotlin/de/patternframeworks/busash/offer/OfferController.kt
@@ -1,28 +1,119 @@
 package de.patternframeworks.busash.offer
 
+import de.patternframeworks.busash.auth.service.JwtTokenServiceImpl
+import de.patternframeworks.busash.user.persistance.UserRepository
+import de.patternframeworks.busash.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/offers")
-class OfferController(@Autowired private val offerRepository: OfferRepository) {
+class OfferController(
+        @Autowired private val offerRepository: OfferRepository,
+        private val jwtTokenService: JwtTokenServiceImpl,
+        private val userRepository: UserRepository,
+        private val userService: UserService
+) {
 
     @GetMapping("")
     fun getAllOffers(): List<Offer> =
-        offerRepository.findAll().toList()
+            offerRepository.findAll().toList()
 
     /**
-     * Returns row count from offer table with corresponding user ID
-     * Used to portray amount of active items offered by user without the need to cache this info
+     * Endpoint to create new offer
+     * @param header Authorization header containing the user token
+     * @param offer Offer details provided in request body
+     * @return ResponseEntity with the created offer and HTTP status code
      * @author Konstantin K.
-     * @param authorId - user ID
-     * @return offer count
+     */
+    @PostMapping("")
+    fun createOffer(
+        @RequestHeader(name = "Authorization") header: String,
+        @RequestBody offer: Offer
+    ): ResponseEntity<Offer> {
+        val token = jwtTokenService.extractTokenFromPrefix(header)
+        val userId = jwtTokenService.getUserIdFromToken(token)
+        val existingUser = userRepository.findById(userId).orElse(null) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        //Extend offer properties from input fields with author_id
+        val offerWithUser = offer.copy(author = existingUser)
+        val newOffer = offerRepository.save(offerWithUser)
+        return ResponseEntity(newOffer, HttpStatus.CREATED)
+    }
+
+    /**
+     * Endpoint to get offer details
+     * @param id offer id as path variable in URL
+     * @return ResponseEntity with the offer and HTTP status code
+     * @author Konstantin K.
      */
     @GetMapping("/{id}")
-    fun getOffersCountByUserId(@PathVariable("id") authorId: Long): Number {
-        return offerRepository.countByAuthorId(authorId)
+    fun getOffer(
+        @PathVariable id: Long
+    ): ResponseEntity<Offer>{
+        val offer = offerRepository.findById(id).orElse(null)
+        return if (offer != null) ResponseEntity(offer, HttpStatus.OK)
+        else ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    /**
+     * Endpoint to delete offer
+     * @param header Authorization header containing the user token
+     * @param id offer id as path variable in URL
+     * @return ResponseEntity with the offer and HTTP status code
+     * @author Konstantin K.
+     */
+    @PutMapping("/{id}")
+    fun updateOffer(
+        @RequestHeader(name = "Authorization") header: String,
+        @PathVariable id: Long,
+        @RequestBody offer: Offer
+    ): ResponseEntity<Any> {
+        val token = jwtTokenService.extractTokenFromPrefix(header)
+        val userId = jwtTokenService.getUserIdFromToken(token)
+        val existingUser = userRepository.findById(userId).orElse(null) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        val existingOffer = offerRepository.findById(id).orElse(null)
+        //Ensure that user can only update their own offers
+        if (existingOffer == null || existingOffer.author != existingUser) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+        val updatedOffer = existingOffer.copy(
+                title = offer.title,
+                description = offer.description,
+                category = offer.category,
+                quantity = offer.quantity,
+                priceType = offer.priceType,
+                price = offer.price,
+                productPic = offer.productPic
+        )
+        offerRepository.save(updatedOffer)
+        return ResponseEntity(updatedOffer, HttpStatus.OK)
+    }
+
+    /**
+     * Endpoint to delete offer
+     * @param header Authorization header containing the user token
+     * @param id offer id as path variable in URL
+     * @return server response and HTTP status code
+     * @author Konstantin K.
+     */
+    @DeleteMapping("/{id}")
+    fun deleteOffer(
+        @RequestHeader(name = "Authorization") header: String,
+        @PathVariable id: Long
+    ): ResponseEntity<Any> {
+        val token = jwtTokenService.extractTokenFromPrefix(header)
+        val userId = jwtTokenService.getUserIdFromToken(token)
+        val existingUser = userRepository.findById(userId).orElse(null) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        val offer = offerRepository.findById(id).orElse(null)
+        //Ensure that user can only delete their own offers
+        if (offer == null || offer.author != existingUser) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+        //Increment transactions counter of a user
+        userService.updateTransactionsCnt(existingUser)
+        offerRepository.deleteById(id)
+        return ResponseEntity("Offer deleted", HttpStatus.OK)
     }
 }
